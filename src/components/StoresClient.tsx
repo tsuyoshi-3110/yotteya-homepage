33a3dd1e8a385492531f053c318c32dbb11a3370
tsuyoshi <66511534+tsuyoshi-3110@users.noreply.github.com>
2025-07-06 17:27:03 +0900

    2025 0706 1727

--- src/components/StoresClient.tsx
+++ src/components/StoresClient.tsx
@@ -359,7 +359,7 @@ export default function StoresClient() {
                 placeholder="紹介文"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
-                className="w-full h-50 border px-3 py-2 rounded"
+                className="w-full border px-3 py-2 rounded"
                 rows={3}
                 disabled={uploading}
               />
