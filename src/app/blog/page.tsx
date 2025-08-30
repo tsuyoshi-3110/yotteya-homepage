06a3a591436be25c86d3e855d88d4872a2651d53
tsuyoshi <66511534+tsuyoshi-3110@users.noreply.github.com>
2025-08-30 10:39:44 +0900

    2025 0830 1039

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -230,7 +230,7 @@ export default function BlogListPage() {
           >
             <path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 2v.01L12 12 4 6.01V6h16zM4 18V8.236l7.386 5.54a1 1 0 0 0 1.228 0L20 8.236V18H4z" />
           </svg>
-          ご連絡はこちらへ
+          取材はこちらへ
           {/* 右矢印（ホバーで少し右へ） */}
           <svg
             viewBox="0 0 24 24"
